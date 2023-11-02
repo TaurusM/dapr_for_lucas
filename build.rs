914266893fd88e832cf98257afa542203073bee5
TaurusM <3075985899@qq.com>
2023-11-02 18:15:07 +0800

    add serde feature

--- build.rs
+++ build.rs
@@ -2,14 +2,18 @@
 
 fn main() -> Result<(), std::io::Error> {
     // env::set_var("OUT_DIR", "src");
-    tonic_build::configure().build_server(true).compile(
-        &[
-            "dapr/proto/common/v1/common.proto",
-            "dapr/proto/runtime/v1/dapr.proto",
-            "dapr/proto/runtime/v1/appcallback.proto",
-            "examples/invoke/proto/helloworld.proto",
-        ],
-        &["."],
-    )?;
+    tonic_build::configure()
+        .client_mod_attribute(".", "use ::serde::{Deserialize, Serialize};")
+        .type_attribute(".", "#[derive(serde::Serialize,serde::Deserialize)]")
+        .build_server(true)
+        .compile(
+            &[
+                "dapr/proto/common/v1/common.proto",
+                "dapr/proto/runtime/v1/dapr.proto",
+                "dapr/proto/runtime/v1/appcallback.proto",
+                "examples/invoke/proto/helloworld.proto",
+            ],
+            &["."],
+        )?;
     Ok(())
 }
